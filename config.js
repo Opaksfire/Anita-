f44a0c812ec744ce7531ff56050f79fcbdf0467f
Izzyblaize <168937431+Izzyblaize@users.noreply.github.com>
2024-08-08 08:02:38 +0100

    config.js

--- config.js
+++ config.js
@@ -6,20 +6,20 @@ global.audio = "";
 global.video = "";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
-global.email = "davidcyril209@gmail.com";
+global.email = "izzyblaize42@gmail.com";
 global.location = "Abuja, Nigeria";
 global.mongodb = process.env.MONGODB_URI || "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
-global.github = process.env.GITHUB || "https://github.com/DeeCeeXxx/Queen_Anita-V2";
+global.github = process.env.GITHUB || "https://github.com/izzyblaize/Queen_Anita-V2";
 global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/17c8ba84a7761eed633f6.jpg";
 global.devs = "https://t.me/deecee_x";
 global.sudo = process.env.SUDO || "";
-global.owner = process.env.OWNER_NUMBER || "";
+global.owner = process.env.OWNER_NUMBER || "2349039547394,2349020387508";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -27,7 +27,7 @@ global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
 global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
-global.userImages = process.env.USER_IMAGES || "https://i.imgur.com/H4qeXwa.jpeg,https://telegra.ph/file/ba9ced500f9eca7db8acb.mp4";
+global.userImages = process.env.USER_IMAGES || "https://imgur.com/a/u8rawaN,https://telegra.ph/file/ba9ced500f9eca7db8acb.mp4";
 global.waPresence = process.env.WAPRESENCE || "available";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
@@ -42,17 +42,17 @@ global.scan = "https://secret-garden-43998-4daad95d4561.herokuapp.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoic1BBWjRVVjQycSsrVVRTRTdmczNwMExEVnZINDZYNFJQVkJUdHdidnJtST0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSi8xek1QekNrZ0VDQUNVd1JmYlJVMGJENVFubFFWUWprTW1Wcy9ybWpXbz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ5RC9PODBjN0hWanZtSVNubVYwTE1rSGl6SDF4eHdRUnpkZzlHc3E1OFU0PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJXakpNVDJuYktIQzJFME9HS1JCRmdMaTlab3hWV1BTRDN0ZVdva0FkL1g4PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjBJVVRtR2NjazFNQ1FiVGhGYTk5ZlBHUmt3RHdlSW1Lc3Vsek1zSGpLbWM9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InRTWjY1VnFDRVY1aEFmamVnQ1NockN4S2YyN0RRQ3FCVjVYdHEzNERSREE9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicUZUMEtTUmd5bFpyMGM1UU5kZnBGMS9ZZlpoQm8rYmNsMXNxK2RvQngxTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOUpGUXl3R0dzTnk1RVlhVkUxU3dxZkpITU0yMklQdzFSMGZxTUxGSkdDOD0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkQyNVowZmRnT1IwRFdXR2JNcEsvUUFoVjhXM0I0RnoxZmFKM01ZZ09ZVWNOekkxVjJMRWJHcmFDNHBtTHNvckZndi9JYjVjQ3JTMmlHT3p3NVdZK2lnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTkyLCJhZHZTZWNyZXRLZXkiOiI4UHgzenFmR2kvZjByNmVMOTgwVU1VQ0JUN1ZnTndBUVhiR2hjUkpyRG4wPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJKc0xyU2ZPUlRGZTNrRFBVYU8wOEp3IiwicGhvbmVJZCI6IjVjYjA0ZDRjLWQ5ZjMtNGE5Ni1hZjY3LTYyZDQ0MTg0YzQwNyIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI5RU9HYktNd0NVSlF4UDY5ZFZMTkxOYXYyQmc9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWnRZNzdTd2R2NEJrRStoSHAvZ3paTFZTYVV3PSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IlJUVDNNUjRGIiwibWUiOnsiaWQiOiIyMzQ5MDM5NTQ3Mzk0OjFAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0t5VzUvRURFSjNFeDdVR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6ImtOZVRoanZCTVlKbzlOd3ZYa1dkT3U3ZHZKdnhxdEg3SU16akpud1BnVVU9IiwiYWNjb3VudFNpZ25hdHVyZSI6IjlKNTlOaWVqSVpTREExem5GY284U2g4ZWtEQXpNSEE0UWp5ZnFuNDJvaFNNcEltcmZ3L3NHWWp0ZlJmSGpRZFpuY1o4bnRRVnNub3lUOC9PcFJyVUFnPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJKbkMzYXdnV043c2NvZE1MN0w0Y3NSb2hyM3ZpejZreHptLzNRYUF2UHNKUXZPU3g5OGpQRi9pUjRhWXJpWmFaQ1ZOWFNiZFVPMEEyRVZVVjhiY0NoZz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjIzNDkwMzk1NDczOTQ6MUBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJaRFhrNFk3d1RHQ2FQVGNMMTVGblRydTNieWI4YXJSK3lETTR5WjhENEZGIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIyOTMzODE1fQ=="
 module.exports = {
   menu: process.env.MENU || "2",
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "`QUEEN_ANITA-V2™`",
-  author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
-  packname: process.env.PACK_NAME || "A N I T A",
-  botname: process.env.BOT_NAME || "QUEEN_ANITA-V2",
-  ownername: process.env.OWNER_NAME || "David Cyril",
+  caption: process.env.CAPTION || "`BYTE BOT™`",
+  author: process.env.PACK_AUTHER || "I Z Z Y",
+  packname: process.env.PACK_NAME || "BTYE COLLECTIONS",
+  botname: process.env.BOT_NAME || "BYTE BOT",
+  ownername: process.env.OWNER_NAME || "Byte Wizard",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -64,7 +64,7 @@ module.exports = {
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
-  LANG: (process.env.THEME || "A N I T A").toUpperCase(),
+  LANG: (process.env.THEME || "B Y T E").toUpperCase(),
 };
 global.rank = "updated";
 global.isMongodb = false;
